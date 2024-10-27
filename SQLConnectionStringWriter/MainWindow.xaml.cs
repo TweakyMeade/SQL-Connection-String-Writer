f754b58d7edd01d930f7e7fbb3d66a7bf7e75887
Anthony Meade <antnymeade@gmail.com>
2024-10-27 22:47:06 +0000

    Add project files.

---
+++ SQLConnectionStringWriter/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using Microsoft.Data.SqlClient;
+namespace SQLConnectionStringWriter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = DataSource.Text;
+            builder.UserID = UserID.Text;
+            builder.Password = Password.Text;
+            builder.InitialCatalog = InitialCatalog.Text;
+            builder.TrustServerCertificate = (bool)TrustServerCertificate.IsChecked;
+            ConnectionString.Text = builder.ConnectionString;
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Clipboard_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(ConnectionString.Text);
+        }
+    }
+}
