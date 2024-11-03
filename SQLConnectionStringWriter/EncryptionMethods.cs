e425f9a8b9fb1310a74ecfb45492363348633012
Anthony Meade <antnymeade@gmail.com>
2024-11-03 22:32:37 +0000

    Starting encryption

---
+++ SQLConnectionStringWriter/EncryptionMethods.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SQLConnectionStringWriter
+{
+    internal class EncryptionMethods
+    { 
+        public static string EncryptXOR(string plaintext, string key)
+        {
+            StringBuilder encrypted = new StringBuilder();
+
+            for (int i = 0; i < plaintext.Length; i++)
+            {
+                encrypted.Append((char)(plaintext[i] ^ key[i % key.Length]));
+            }
+
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(encrypted.ToString()));
+        }
+        public static string DecryptXOR(string encryptedText, string key)
+        {
+            string decodedString = Encoding.UTF8.GetString(Convert.FromBase64String(encryptedText));
+            StringBuilder decrypted = new StringBuilder();
+
+            for (int i = 0; i < decodedString.Length; i++)
+            {
+                decrypted.Append((char)(decodedString[i] ^ key[i % key.Length]));
+            }
+
+            return decrypted.ToString();
+        }
+        public static string EncryptAES(string plainText, string key, string iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+        public static string DecryptAES(string cipherText, string key, string iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(iv);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        return srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+
+}
